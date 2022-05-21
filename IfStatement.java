f9111cb6be94ecdae54b39ccb05f73c7284ca8e2
AdiCahyaSaputra <adi.JSONparse@gmail.com>
2022-05-21 15:26:03 +0700

    if statement, ternary operator

---
+++ IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+	public static void main(String[] args) {
+		int nilai = 70;
+		int absen = 95;
+
+		if(nilai >= 70 && absen >= 80) {
+			System.out.println("Wah Kamu Lulus! Hebat sekali >_");
+		} else {
+			System.out.println("Coba lagi tahun depan :)");
+		}
+	}
+}
---
+++ Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+	public static void main(String[] args) {
+		// btw dead code di fasly an nya karena si javac tau klo kondisi nya tuh udh true, jadi yg di false nya g guna dan menuh menuhin memori 		
+		String hasil = 7 + 6 > 12 ? "Bener Banget" : "Salah Hehe";
+		System.out.println(hasil);
+	}
+}
